7281b56edb2bd6bad5daaf1459680337452d585b
Kyle Vu <ktvu@hbuhsd.edu>
2025-05-04 01:07:27 +0000

    initial commit

--- Input01.java
+++ Input01.java
@@ -1,13 +1,8 @@
-import javax.swing.JOptionPane;
-
 public class Input01 {
     public static void main(String[] args) {
         //Create a JOptionPane.
         //Store the input as a String and print it.
-        String input = JOptionPane.showInputDialog("Give number:");
-        int num = Integer.parseInt(input);
-        num++;
-        System.out.println("Adding one to your number.  It is now: " + num);
+        
         
         
         //Parse the input as an int.
